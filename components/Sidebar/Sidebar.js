6a1d1c16701c283ee8388d6faaa64f57c337669c
Khushi Sharma <87928896+Khushi21S@users.noreply.github.com>
2025-04-23 15:01:20 +0530

    sidebar changes

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -8,10 +8,19 @@ import Link from "next/link";
 
 const Sidebar = ({ closeSidebar }) => {
   const navItems = [
-    { label: "Home", target: "#home" },
-    { label: "About me", target: "#about" },
-    { label: "Experience", target: "#experience" },
-    { label: "Projects", target: "#projects" },
+    { label: "Home", target: "#home", backgroundColor: "rgb(234 179 8 / var(--tw-bg-opacity, 1))" },
+    { label: "About me", target: "#about",backgroundColor: "rgb(59 130 246 / var(--tw-bg-opacity, 1))" },
+    { label: "Experience", target: "#experience",backgroundColor: "rgb(20 184 166 / var(--tw-bg-opacity, 1))" },
+    { label: "Projects", target: "#projects", backgroundColor: "rgb(99 102 241 / var(--tw-bg-opacity, 1))" },
+  ];
+  const socialItems = [
+    { label: "Github", redirectLink: "https://github.com/Khushi21S" },
+    {
+      label: "LinkedIn",
+      redirectLink: "https://www.linkedin.com/in/khushi-sharma-971bb7209/",
+    },
+    { label: "Leetcode", redirectLink: "https://leetcode.com/u/khushi21S/" },
+    { label: "Twitter", redirectLink: "https://x.com/basaajaurkal" },
   ];
   return (
     <>
@@ -30,20 +39,51 @@ const Sidebar = ({ closeSidebar }) => {
         transition={{ type: "tween", duration: 0.3 }}
       >
         <div className={styles.closeIcon}>
-          <Image width={25} height={25} src={crossIcon} onClick={closeSidebar} alt="crossIcon" />
+          <Image
+            width={25}
+            height={25}
+            src={crossIcon}
+            onClick={closeSidebar}
+            alt="crossIcon"
+          />
         </div>
-        <nav className={styles.nav}>
-          {navItems.map(({ label, target }) => (
-               <Link
-               key={target}
-               href={target}
-               onClick={closeSidebar}
-    
-             >
-               <span>{label}</span>
-             </Link>
-          ))}
-        </nav>
+        <div className={styles.navItems}>
+          <div>
+          <div>
+            <span>SOCIAL</span>
+            <nav className={styles.nav}>
+              {socialItems.map(({ label, redirectLink }) => (
+                <a
+                  key={redirectLink}
+                  href={redirectLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={closeSidebar}
+                >
+                  <span>{label}</span>
+                </a>
+              ))}
+            </nav>
+          </div>
+          <div>
+            <span>MENU</span>
+            <nav className={styles.nav}>
+              {navItems.map(({ label, target,backgroundColor }) => (
+                <Link key={target} href={target} onClick={closeSidebar}>
+                  <span>
+                    <span style={{backgroundColor: backgroundColor}} className={styles.coloredBall}></span>
+                    {label}
+                  </span>
+                </Link>
+              ))}
+            </nav>
+          </div>
+          </div>
+          <div className={styles.contactDetails}>
+            <p className={styles.titleText}>GET IN TOUCH</p>
+            <p>worksharma.khushi21@gmail.com</p>
+          </div>
+        </div>
       </motion.div>
     </>
   );
